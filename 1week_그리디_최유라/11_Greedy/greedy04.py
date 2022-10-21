a45b3296e5e40582f0c03bd2b34d71b80b61aaed
arooy <yoora213@gmail.com>
2022-10-21 17:23:50 +0900

    4_Implementation_1021최유라

--- 1week_그리디_최유라/11_Greedy/greedy04.py
+++ 1week_그리디_최유라/11_Greedy/greedy04.py
@@ -26,3 +26,18 @@ from itertools import *
 #         break
 #     if(data.count(result) > 0):
 #         result += 1
+
+# 책 해설
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+target = 1
+for x in data:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < x:
+        break
+    target += x
+
+# 만들 수 없는 금액 출력
+print(target)
